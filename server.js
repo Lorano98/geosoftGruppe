f990684a145e2da99e0f621e2d9493adf1b9ed1f
Lorano98 <evalangstein@freenet.de>
2022-06-13 13:08:49 +0200

    zeug

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ let port = 5000;
 let server = http.createServer(handleRequest); // create a server
 server.listen(port, host); // say where the server should listen
 // say what the server should do, when it gets an incoming request
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017'; // connection URL
+const client = new MongoClient(url); // mongodb client
+
 function handleRequest(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Helloooooooooooo');
